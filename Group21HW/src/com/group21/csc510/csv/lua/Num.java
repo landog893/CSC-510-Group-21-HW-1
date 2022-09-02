9fbb56a6099aa63240ca2c8d9a5d84984653bd9c
saminur <saminur.islam08@gmail.com>
2022-09-01 22:06:42 -0400

    make the structural changes

---
+++ Group21HW/src/com/group21/csc510/csv/lua/Num.java
@@ -0,0 +1,20 @@
+package com.group21.csc510.csv.lua;
+
+/**
+ * 
+ */
+
+/**
+ * @author Shruti Marota
+ * @author Landon Gaddy
+ * @author Jerry Chang
+ * @author Jesse Chen
+ * @author Sami Islam
+ * 
+ * Feel free to change author tags if needed
+ *
+ */
+
+public class Num {
+
+}
