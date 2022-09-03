46789ed1c1d37d14bcec371a5890a6e79954ed70
smarota17 <shrutimarota@gmail.com>
2022-09-03 11:24:05 -0400

    Pushing Sym class changes.

--- Group21HW/src/com/group21/csc510/csv/lua/Sym.java
+++ Group21HW/src/com/group21/csc510/csv/lua/Sym.java
@@ -1,5 +1,8 @@
 package com.group21.csc510.csv.lua;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 
  */
@@ -16,5 +19,73 @@ package com.group21.csc510.csv.lua;
  */
 
 public class Sym {
-
+	
+	private int n;
+	private int at;
+	private String name;
+	private HashMap<String,Integer> _has;
+	
+//	function Sym:new(c,s) 
+//	  return {n=0,          -- items seen
+//	          at=c or 0,    -- column position
+//	          name=s or "", -- column name
+//	          _has={}       -- kept data
+//	         } end
+	/**
+	 * Sym constructor.
+	 * @param c int for column position
+	 * @param s String for s 
+	 */
+	public Sym(int c, String s){
+		this.n = 0;
+		this.at = c; // unsure about at=c or 0
+		this.name = s;
+		this._has= new HashMap<String, Integer>();
+	}
+	
+//	  -- ## Sym
+//	  -- Add one thing to `col`. For Num, keep at most `nums` items.
+//	  function Sym:add(v)
+//	    if v~="?" then self.n=self.n+1; self._has[v]= 1+(self._has[v] or 0) end end
+	/**
+	 * Method to add something to col. 
+	 * @param v
+	 */
+	public void add(String v) {
+		if (!v.equals("?")) {
+			this.n = this.n + 1;
+			// if the key is already in the hashmap, increment the value
+			if (this._has.get(v) != null) {
+				int currentValue = this._has.get(v);
+				this._has.put(v, (1 + currentValue));
+			}
+			// if the key is not already in the hashmap, add with 0 as the value
+			else {
+				this._has.put(v, 0);
+			}
+		}
+	}
+	
+//	  function Sym:mid(col,    most,mode) 
+//	    most=-1; for k,v in pairs(self._has) do if v>most then mode,most=k,v end end
+//	    return mode end 
+		
+	public int mid(String col, int most, String mode) {
+		most = -1;
+		
+		for (Map.Entry<String, Integer> entry : this._has.entrySet()) {
+		    String key = entry.getKey();
+		    Integer value = entry.getValue();
+		    if (value > most) {
+		    	mode = key;
+		    	most = value;
+		    }
+		}
+		return Integer.parseInt(mode);
+	}
+//	  function Sym:div(    e,fun)
+//	    function fun(p) return p*math.log(p,2) end
+//	    e=0; for _,n in pairs(self._has) do if n>0 then e=e - fun(n/self.n) end end
+//	    return e end 
+	
 }
