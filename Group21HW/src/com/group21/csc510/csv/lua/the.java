6826a95f01050ef445382098441f26d6e5abe24a
saminur <saminur.islam08@gmail.com>
2022-09-02 20:34:16 -0400

    Making changes in the Num file for the add function. Create necessary class files for that as well.

---
+++ Group21HW/src/com/group21/csc510/csv/lua/the.java
@@ -0,0 +1,22 @@
+package com.group21.csc510.csv.lua;
+
+import java.util.Dictionary;
+
+public class the {
+	
+	private Dictionary nums;
+	
+	public the(Dictionary nums) {
+		this.nums=nums;
+	}
+
+	  // Getter
+	  public Dictionary getNums() {
+	    return nums;
+	  }
+
+	  // Setter
+	  public void setName(Dictionary nums) {
+	    this.nums = nums;
+	  }
+}
