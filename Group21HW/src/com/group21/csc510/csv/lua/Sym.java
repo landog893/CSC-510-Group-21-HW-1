e0ea8a8ceb5026e1640bcd6c62815b26073eb28d
Huangxing Chen <xingye1031@gmail.com>
2022-09-02 23:19:21 -0400

    Modified Sym

--- Group21HW/src/com/group21/csc510/csv/lua/Sym.java
+++ Group21HW/src/com/group21/csc510/csv/lua/Sym.java
@@ -1,5 +1,7 @@
 package com.group21.csc510.csv.lua;
 
+import java.util.*;
+
 /**
  * 
  */
@@ -16,5 +18,38 @@ package com.group21.csc510.csv.lua;
  */
 
 public class Sym {
+	int numItems;
+	int columnPosition;
+	String columnName;
+	Map<Integer, Integer> _has = new HashMap<>();
+	
+	
+	public int mid() {
+		int most = -1;
+		int mode = -1;
+		for (Map.Entry<Integer, Integer> entry: this._has.entrySet()) {
+			if(entry.getValue()>most) {
+				mode = entry.getKey();
+				most = entry.getValue();			
+			}
+		}
+		
+		return mode;
+	}
+	
+	public double div() {
+		double e = 0;
+		for (Map.Entry<Integer, Integer> entry: this._has.entrySet()) {
+			int n = entry.getValue();
+			if(n>0) {
+				e = e - this.divFun(n/this.numItems);
+			}
+		}
+		return e;
+	}
+	
+	private double divFun(int n) {
+		return n*(Math.log(n)/ Math.log(2));
+	}
 
 }
