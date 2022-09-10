2653b8dfd612a3760658a0651394610a14b6c198
saminur <saminur.islam08@gmail.com>
2022-09-10 14:53:40 -0400

    Data class constructor and Add function

---
+++ Group21HW/src/com/group21/csc510/csv/lua/Cols.java
@@ -0,0 +1,7 @@
+package com.group21.csc510.csv.lua;
+
+public class Cols{
+
+
+
+}
--- Group21HW/src/com/group21/csc510/csv/lua/Data.java
+++ Group21HW/src/com/group21/csc510/csv/lua/Data.java
@@ -1,12 +1,69 @@
 package com.group21.csc510.csv.lua;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class Data {
-	Cols cols;
-	int[] rows;
+	private Cols cols;
+	private ArrayList<Row> rows;
 	
-	
+	public static boolean isPathvalid(String path) {
+		try {
+			Paths.get(path);
+		} catch (InvalidPathException e) {
+			// TODO: handle exception
+			return false;
+		}
+		
+		return true;
+	}
+	public Data(String src) {
+		
+		this.cols = null;
+		this.rows = new ArrayList<Row>();
+		
+		if(isPathvalid(src)) {
+			try {
+				Scanner sc = new Scanner(new File((String) src));
+				while(sc.hasNext()) {
+					String row = sc.next();
+					this.add(row);
+				}
+			} catch (FileNotFoundException e) {
+				// TODO Auto-generated catch block
+				System.out.println(e.getMessage());
+			}
+			
+		}
+		else {
+			Scanner sc = new Scanner(src);
+			while(sc.hasNext()) {
+				  String row = sc.next();
+				  this.add(row);
+			}
+		}
+		
+	}
+	public void add(String xs,String row) {
+		if (this.cols == null) {
+			this.cols = Cols(xs);
+		}
+		else {
+			Row row_ = Row(xs);
+			this.rows.add(row_);
+			for(int i = 0; i < this.cols.y.size(); i++) {
+				ArrayList<Cols> col = this.cols.y.get(i);
+				for (int j = 0, j< col.size(); j++) {
+					// how we will do the polimorphism here
+				}
+			}
+		}
+		
+		
+	}
 	public HashMap<String, Object> stats(int places, List<List<Integer>> showCols, String fun){
 		if(showCols == null) showCols = cols.y;
 		if(fun == null) fun = "mid";
