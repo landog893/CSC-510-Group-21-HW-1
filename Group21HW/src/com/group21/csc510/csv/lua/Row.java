cd290872aca3c4932854e1ccce90568736cdbf8e
saminur <saminur.islam08@gmail.com>
2022-10-03 18:31:12 -0400

    Put Java doc in Row class

--- Group21HW/src/com/group21/csc510/csv/lua/Row.java
+++ Group21HW/src/com/group21/csc510/csv/lua/Row.java
@@ -2,12 +2,24 @@ package com.group21.csc510.csv.lua;
 
 
 import java.util.*;
-
+/**
+ * Class to represent row which holds one record from the CSV
+ * @author Landon Gaddy
+ */
 public class Row {
+	
 	HashMap<String, Object> rowBuilder;
+	/** String Array holds one record in each cells **/
 	String [] cells;
+	/** String Array which is used if we discretize the data**/
 	String [] cooked;
+	/** a boolan value return true if y -values evaluated **/
 	boolean isEvaled;
+	
+	/**
+	 * Constructor for the Cols object.
+	 * @param t for passing the record from csv
+	 */
 	public Row(String [] t){
 		cells = t;
 		cooked = Arrays.copyOf(t,t.length);
